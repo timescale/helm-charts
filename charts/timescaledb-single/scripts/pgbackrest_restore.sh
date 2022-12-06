4a56e09c76f514559f8d4f6733b14223c918b199
Paweł Krupa (paulfantom) <pawel@krupa.net.pl>
2022-12-06 15:21:54 +0100

    chart/timescaledb-single: move pgbackrest_restore.sh out of ConfigMap
    Signed-off-by: Paweł Krupa (paulfantom) <pawel@krupa.net.pl>

---
+++ charts/timescaledb-single/scripts/pgbackrest_restore.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# PGBACKREST_BACKUP_ENABLED variable is passed in StatefulSet template
+[ "${PGBACKREST_BACKUP_ENABLED}" = "true" ] || exit 1
+
+: "${ENV_FILE:=${HOME}/.pod_environment}"
+if [ -f "${ENV_FILE}" ]; then
+echo "Sourcing ${ENV_FILE}"
+. "${ENV_FILE}"
+fi
+
+# PGDATA and WALDIR are set in the StatefulSet template and are sourced from the ENV_FILE
+# PGDATA=
+# WALDIR=
+
+# A missing PGDATA points to Patroni removing a botched PGDATA, or manual
+# intervention. In this scenario, we need to recreate the DATA and WALDIRs
+# to keep pgBackRest happy
+[ -d "${PGDATA}" ] || install -o postgres -g postgres -d -m 0700 "${PGDATA}"
+[ -d "${WALDIR}" ] || install -o postgres -g postgres -d -m 0700 "${WALDIR}"
+
+exec pgbackrest --force --delta --log-level-console=detail restore
